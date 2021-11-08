af2ed89471de2f9304f124b91d3a373875aaef9f
Minkyu3 <gkhtls2006@broadwave.co.kr>
2021-11-08 15:51:11 +0900

    엔티티

---
+++ src/main/java/com/broadwave/toppos/Head/Branoh/Branoh.java
@@ -0,0 +1,71 @@
+package com.broadwave.toppos.Head.Branoh;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Minkyu
+ * Date : 2021-11-08
+ * Time :
+ * Remark : Toppos 지사 테이블
+ */
+@Entity
+@Data
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="bs_branoh")
+public class Branoh {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="br_id")
+    private Long id;
+
+    @Column(unique = true,name="br_code")
+    private String brCode; // 지점코드 2자리
+
+    @Column(name="br_name")
+    private String brName; // 지점명
+
+    @Column(name="br_contract_dt")
+    private String brContractDt; // 계약일자
+
+    @Column(name="br_contract_form_dt")
+    private String brContractFormDt; // 계약기간 from
+
+    @Column(name="br_contract_to_dt")
+    private String brContractToDt; // 계약기간 to
+
+    @Column(name="br_contract_state")
+    private String brContractState; // 진행중 : 01, 계약완료 : 02
+
+    @Column(name="br_carculate_rate_hq")
+    private Double brCarculateRateHq; // 정산비율(본사)
+
+    @Column(name="br_carculate_rate_br")
+    private Double brCarculateRateBr; // 정산비율(지사)
+
+    @Column(name="br_carculate_rate_fr")
+    private Double brCarculateRateFr; // 정산비율(가맹점)
+
+    @Column(name="br_remark")
+    private String brRemark; // 특이사항
+
+    @Column(name="insert_id")
+    private String insert_id;
+
+    @Column(name="insert_date")
+    private LocalDateTime insertDateTime;
+
+    @Column(name="modify_id")
+    private String modify_id;
+
+    @Column(name="modify_date")
+    private LocalDateTime modifyDateTime;
+
+}
