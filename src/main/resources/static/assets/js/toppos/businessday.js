e84778d3276ca56ffd703aa3024506a696b039a2
snw <snw@broadwave.co.kr>
2021-11-19 14:35:54 +0900

    영업일 관리 변경한 값 받아오기까지

--- src/main/resources/static/assets/js/toppos/businessday.js
+++ src/main/resources/static/assets/js/toppos/businessday.js
@@ -4,17 +4,54 @@ $(function() {
     createGrids();
 
     setDataIntoGrid();
-    /* 0번그리드 내의 셀 클릭시 이벤트 */
-    AUIGrid.bind(gridId[0], "cellClick", function (e) {
 
+
+    $(".aui-grid-default-column").on("mouseenter", function (e){
+        if(isDrag) {
+            $(e.target).trigger("click");
+        }
     });
+
+    $(".aui-grid-default-column").on("mousedown", function (e){
+        $(e.target).trigger("click");
+    });
+
+    $(window).on("mousedown", function (){
+        isDrag = true;
+    });
+
+    $(window).on("mouseup", function (){
+        isDrag = false;
+    });
+
+
+    for(let i = 0; i <12; i++) {
+        AUIGrid.bind(gridId[i], "cellClick", function( event ) {
+            let item = event.item;
+            let value = item[event.columnIndex];
+            if(value !== undefined) {
+                let isSelected = value[8];
+                if(isSelected !== undefined) {
+                    if (isSelected === "Y") {
+                        item[event.columnIndex] = value.substr(0, 8) + "N";
+                    } else {
+                        item[event.columnIndex] = value.substr(0, 8) + "Y";
+                    }
+                    AUIGrid.updateRow(gridId[i], item, event.rowIndex);
+                }
+            }
+        });
+    }
+
 });
 
+let isDrag = false;
+let isSelected = false;
 
 /* 그리드 생성과 운영에 관한 중요 변수들. grid라는 이름으로 시작하도록 통일했다. */
 let gridId = [];
 let gridTargetDiv = [];
-
+let gridData = [];
 /* 그리드를 뿌릴 대상 div의 id들 */
 gridTargetDiv = [
     "grid_calendar_0", "grid_calendar_1", "grid_calendar_2", "grid_calendar_3", "grid_calendar_4", "grid_calendar_5",
@@ -29,24 +66,38 @@ let gridColumnLayout = [
     {
         dataField: "0",
         headerText: "일",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "1",
         headerText: "월",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "2",
         headerText: "화",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "3",
         headerText: "수",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "4",
         headerText: "목",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "5",
         headerText: "금",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }, {
         dataField: "6",
         headerText: "토",
+        labelFunction : showDay,
+        styleFunction : selectedStyle,
     }
 ];
 
@@ -55,11 +106,11 @@ let gridColumnLayout = [
 * */
 gridProp = {
     editable : false,
-    selectionMode : "multipleCells",
+    selectionMode : "none",
     noDataMessage : "출력할 데이터가 없습니다.",
     enableColumnResize : false,
     showRowNumColumn : false,
-    enableFilter : true
+    enableFilter : false
 };
 
 
@@ -72,46 +123,74 @@ function createGrids() {
 
 /* 한자리 숫자일 경우 앞에 0을 붙여 두자리로 해준다. */
 function pad2(number) {
-    return (number < 10 ? '0' : '') + number
+    return (number < 10 ? '0' : '') + number;
 }
 
 /* ajax 통신을 통해 그리드 데이터를 받아와 뿌린다. */
 function setDataIntoGrid() {
 
-    let gotData = [];
-    for(let i = 0; i < 12; i++) {
-        for(let j = 0; j < 30; j++) {
-            gotData.push({date : "2021" + this.pad2(i) + this.pad2(j)});
-        }
-    }
-    const firstDay = new Date(2021, 1, 1);
-    let dayOfWeek = firstDay.getDay();
+    let targetYear = 2021;
+    let firstDayOfWeek = [];
+    let lastDayOfMonth = [];
+    let processDayOfWeek;
+    let processData = {};
+    let processDataOfMonth = [];
 
-    let currentMonth = 1;
-    let processingData = [];
-    let processedData = [];
-
-
-
-    for(let i = 1; i < 13; i++) {
-        for(let j = 1; j < 32; j++) {
-            if(dayOfWeek === 7) {
-                processedData[i-1].push(processingData);
-                processingData = [];
-                dayOfWeek = 0;
+    for(let m = 1; m <= 12; m++) {
+        firstDayOfWeek[m-1] = new Date(targetYear, m-1, 1).getDay();
+        lastDayOfMonth[m-1] = new Date(targetYear, m, 0).getDate();
+        processDayOfWeek = firstDayOfWeek[m-1];
+        for(let d = 1; d <= lastDayOfMonth[m-1]; d++) {
+            processData[processDayOfWeek+""] = targetYear+pad2(m)+pad2(d)+"N";
+            if(processDayOfWeek >= 6 || d===lastDayOfMonth[m-1]) {
+                processDataOfMonth.push(processData);
+                processDayOfWeek = 0;
+                processData = {};
+            }else{
+                processDayOfWeek++;
             }
-            processingData.push();
-            parseInt(data[i].date.substr(4, 2), 10);
         }
+        gridData[m-1] = processDataOfMonth;
+        AUIGrid.setGridData(gridId[m-1], gridData[m-1]);
+        processDataOfMonth = [];
     }
+}
 
-    /*
-    CommonUI.ajax(url, "GET", false, function (req) {
-    });
-    */
+
+
+function showDay(rowIndex, columnIndex, value, headerText, item) {
+    let resultValue = "";
+    if(value !== undefined) {
+        resultValue = parseInt(value.substr(6, 2), 10) + "";
+        isSelected = value[8] === "Y";
+    }else{
+        isSelected = false;
+    }
+    return resultValue;
+}
+
+function selectedStyle(value, footerValues) {
+    const setStyle = isSelected ? "cellRed" : "cellWhite";
+    return setStyle;
 }
 
 /*  */
 function gridSave() {
 
+    let data = [];
+
+    for(let m = 0; m < 12; m++) {
+        const updatedItems = AUIGrid.getEditedRowColumnItems(gridId[m]);
+        for(let i = 0; i < updatedItems.length; i++) {
+            for(let j = 0; j < 7; j++) {
+                const rawData = updatedItems[i][j];
+                if(rawData !== undefined) {
+                    data.push({date : rawData.substr(0,8), selected : rawData[8]});
+                }
+            }
+        }
+        AUIGrid.resetUpdatedItems(gridId[m]);
+    }
+
+    console.log(data);
 }
