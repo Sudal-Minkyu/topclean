4ba68861f20208a9ec37a1f9d8b045912713d454
snw <snw@broadwave.co.kr>
2021-12-01 09:40:40 +0900

    itemgroup : 아이템 사용여부 기능 롤백

--- src/main/resources/static/assets/js/toppos/itemgroup.js
+++ src/main/resources/static/assets/js/toppos/itemgroup.js
@@ -300,6 +300,7 @@ function setDataIntoGrid(numOfGrid, url, code = false) {
 
 /* 지정된 그리드에 새 빈 데이터 추가 */
 function addRow(numOfGrid) {
+    let item = {};
     switch (numOfGrid) {
         case 0 :
             AUIGrid.addRow(gridId[numOfGrid], {bgUseYn : "Y"}, "last");
@@ -308,8 +309,9 @@ function addRow(numOfGrid) {
             item = AUIGrid.getSelectedRows(gridId[0]);
             if(item.length && !AUIGrid.isAddedById(gridId[0], item[0]._$uid)) {
                 delete item[0]['_$uid'];
+                item['bsUseYn'] = "Y";
                 console.log(item);
-                AUIGrid.addRow(gridId[numOfGrid], {bsUseYn : "Y"}, "last");
+                AUIGrid.addRow(gridId[numOfGrid], item, "last");
             }else{
                 alertCaution("저장되어 있는 대분류 항목을 선택해 주세요", 1);
             }
@@ -318,7 +320,8 @@ function addRow(numOfGrid) {
             item = AUIGrid.getSelectedRows(gridId[1]);
             if(item.length && !AUIGrid.isAddedById(gridId[1], item[0]._$uid)) {
                 delete item[0]['_$uid'];
-                AUIGrid.addRow(gridId[numOfGrid], {biUseYn : "Y"}, "last");
+                item['biUseYn'] = "Y";
+                AUIGrid.addRow(gridId[numOfGrid], item, "last");
             }else{
                 alertCaution("저장되어 있는 중분류 항목을 선택해 주세요", 1);
             }
