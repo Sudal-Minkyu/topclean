27f82c74b230f2211ff3c0d933590d6a073b3bb2
snw <snw@broadwave.co.kr>
2021-12-22 11:17:02 +0900

    pricemanagement : 숫자에 대한 , 처리 및 예외처리 CommonUI : toInt() 함수 예외처리 강화

--- src/main/resources/static/assets/js/toppos/head/pricemanagement.js
+++ src/main/resources/static/assets/js/toppos/head/pricemanagement.js
@@ -1,12 +1,19 @@
 $(function () {
     CommonUI.ajax("/api/head/addCostInfo", "GET", {}, function (req) {
         Object.keys(req.sendData.addCostDto).forEach(id => {
-           $("#" + id).val(req.sendData.addCostDto[id]);
+           $("#" + id).val(req.sendData.addCostDto[id].toLocaleString());
         });
+    });
+    $("#priceManagementForm input").on("keyup", function (e) {
+        this.value = this.value.toInt().toLocaleString();
     });
 });
 
 function onSave() {
+    const inputs = $("#priceManagementForm input");
+    inputs.each(function (index, item) {
+        item.value = item.value.toInt();
+    });
     const formData = new FormData(document.getElementById('priceManagementForm'));
 
     let url = "/api/head/addCostUpdate";
@@ -14,4 +21,9 @@ function onSave() {
     CommonUI.ajax(url, "POST", formData, function (req){
         alertSuccess("가격 기초정보 저장 완료");
     });
+
+    inputs.each(function (index, item) {
+        item.value = item.value.toInt().toLocaleString();
+    });
+
 }
