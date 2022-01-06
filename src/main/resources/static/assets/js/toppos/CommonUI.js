64b676d45bf7093656c7eda4494e96449d87c7fe
snw <snw@broadwave.co.kr>
2022-01-06 13:11:54 +0900

    jest를 위한 라이브러리 파일과 잔여 코드들 삭제 intergrate : 상품수정기능 옮기는중

--- src/main/resources/static/assets/js/toppos/CommonUI.js
+++ src/main/resources/static/assets/js/toppos/CommonUI.js
@@ -313,14 +313,3 @@ class CommonUIClass {
         });
     }
 }
-
-let CommonUI = new CommonUIClass();
-
-/* jest 테스트를 위해 nodejs 의 요소에 테스트가 필요한 기능을 탑재하여 내보내기 한다. 브라우저 실행 환경에서는 무시 처리 된다. */
-try {
-    module.exports = CommonUIClass;
-}catch (e) {
-    if(!(e instanceof ReferenceError)) {
-        console.log(e);
-    }
-}
