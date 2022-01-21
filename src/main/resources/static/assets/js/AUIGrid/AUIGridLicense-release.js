4c25d4514304bb76ec1780aa9596ea0b699a3526
cis-WIn <kr.chis@gmail.com>
2022-01-21 15:04:18 +0900

    AUI Grid 정식 라이선스파일 개발용, release용

---
+++ src/main/resources/static/assets/js/AUIGrid/AUIGridLicense-release.js
@@ -0,0 +1,8 @@
+/**
+ * AUIGrid 3.0.11 License
+ * License Type : Enterprise License
+ * Product ID : AUIGrid-EN-19832-669111-211025
+ * Authorized Domain(or IP) : pos.topcleaners.kr
+ */
+/* eslint-disable */
+AUIGridLicense = "eyJjdCI6Iko0ZXBROGJkRlRoQ0swamdZTkRkS0lJVlNKSW9mcnhZUXNsNWkzeUlKSXZ1andlQUk2Y0xneEczTnE0VlA3eXdqOFNQMmxoaHlRVUJhWS9jRVdlSjQ0RWdwcTlMQ0x5VzRmYW01M2NtWjYvNjl2cVZyWEN2OTlWejNRYi9GMHZHIiwiaXYiOiI2YjlhNmQ1MmVjYjJkMGY1OGI5MzdjYzMyOTg1MzQzZiIsInMiOiJiNzFkNjc2ODBjZmVmZWI1In0=";
