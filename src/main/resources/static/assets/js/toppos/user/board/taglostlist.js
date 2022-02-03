0e60d1b0c54faf7c8aa23445895663eb5c0fc285
snw <snw@broadwave.co.kr>
2022-02-03 13:24:38 +0900

    taglostlist : 게시목록에 필요한 기본 필요 데이터 정의

---
+++ src/main/resources/static/assets/js/toppos/user/board/taglostlist.js
@@ -0,0 +1,223 @@
+/* 서버 API와 주고 받게 될 데이터 정의
+* "s" 문자형, "n" 숫자형, "r" 필수값, "d" 불필요한 데이터 삭제(receive에 있을 경우 앞으로도 불필요할 경우에는 API에서 삭제요청할것)
+* 조합하여 "sr", "nr" 같은 형식도 가능
+* 추가로 필요한 검사항목이 생긴다면 문의 바랍니다.
+* */
+const dtos = {
+    send: {
+        페이지에따른게시판데이터받아오기: {
+            page: "nr",
+            rowCount: "nr", // 한 페이지당 보여줄 행 수
+        }
+    },
+    receive: {
+        페이지에따른게시판데이터받아오기: {
+            작성글의리스트: {
+                id: "nr", // 게시판 글 id
+                subject: "s",
+                name: "s",
+                insertDt: "s", // yyyymmdd만
+            },
+            페이징필요정보: {
+                totalRowCount: 0, // 전체 게시물 건수
+            },
+        }
+    }
+};
+
+/* 통신에 사용되는 url들 기입 */
+const urls = {
+    
+}
+
+/* 서버 API를 AJAX 통신으로 호출하며 커뮤니케이션 하는 함수들 (communications) */
+const comms = {
+    
+};
+
+/* .s : AUI 그리드 관련 설정들
+*  같은 번호의 배열에 있는 요소들 끼리 철저하게 연동한다는 원칙을 따른다.
+*  어쩔 수 없이 한 그리드에 여러개의 요소가 필요할 경우 다차원 배열을 통해 구현한다.
+*  .f : 그리드 관련 함수들 배치
+*  .e : 그리드 객체에 걸리는 이벤트들 배치
+* */
+const grids = {
+    s: { // 그리드 세팅
+        targetDiv: [
+            "grid_list"
+        ],
+        columnLayout: [],
+        prop: [],
+        id: [],
+        data: [],
+    },
+
+    f: { // 그리드 펑션
+        initialization() { // 가시성을 위해 grids.s 의 일부 요소를 여기서 선언한다.
+
+            /* 0번 그리드의 레이아웃 */
+            grids.s.columnLayout[0] = [
+                {
+                    dataField: "htSubject",
+                    headerText: "제목",
+                }, {
+                    dataField: "",
+                    headerText: "작성자",
+                }, {
+                    dataField: "insertDt",
+                    headerText: "작성일",
+                },
+            ];
+
+            /* 0번 그리드의 프로퍼티(옵션) 아래의 링크를 참조
+            * https://www.auisoft.net/documentation/auigrid/DataGrid/Properties.html
+            * */
+            grids.s.prop[0] = {
+                editable : false,
+                selectionMode : "none",
+                noDataMessage : "출력할 데이터가 없습니다.",
+                showAutoNoDataMessage: false,
+                enableColumnResize : false,
+                showRowNumColumn : false,
+                showStateColumn : false,
+                enableFilter : false,
+                rowHeight : 48,
+                headerHeight : 48,
+            };
+
+        },
+
+        create() { // 그리드 동작 처음 빈 그리드를 생성
+            for (const i in grids.s.columnLayout) {
+                grids.s.id[i] = AUIGrid.create(grids.s.targetDiv[i], grids.s.columnLayout[i], grids.s.prop[i]);
+            }
+        },
+
+        getData(numOfGrid) { // 해당 배열 번호 그리드의 url.read 를 참조하여 데이터를 그리드에 뿌린다.
+            return AUIGrid.getGridData(grids.s.id[numOfGrid]);
+        },
+
+        setData(numOfGrid, data) { // 해당 배열 번호 그리드의 url.read 를 참조하여 데이터를 그리드에 뿌린다.
+            AUIGrid.setGridData(grids.s.id[numOfGrid], data);
+        },
+
+        clearData(numOfGrid) { // 해당 배열 번호 그리드의 데이터 비우기
+            AUIGrid.clearGridData(grids.s.id[numOfGrid]);
+        },
+
+        resize(num) { // 해당 배열 번호 그리드의 크기를 현제 그리드를 감싼 엘리먼트에 맞춰 조절
+			AUIGrid.resize(grids.s.id[num]);
+		},
+
+        getCheckedItems(numOfGrid) { // 해당 배열 번호 그리드의 엑스트라 체크박스 선택된 (아이템 + 행번호) 객체 반환
+            return AUIGrid.getCheckedRowItems(grids.s.id[numOfGrid]);
+        },
+
+        
+    },
+
+    t: {
+        basicTrigger() {
+            /* 0번그리드 내의 셀 클릭시 이벤트 */
+            AUIGrid.bind(grids.s.id[0], "cellClick", function (e) {
+                console.log(e.item); // 이밴트 콜백으로 불러와진 객체의, 클릭한 대상 row 키(파라메터)와 값들을 보여준다.
+            });
+        }
+    }
+};
+
+/* 이벤트를 s : 설정하거나 r : 해지하는 함수들을 담는다. 그리드 관련 이벤트는 grids.e에 위치 (trigger) */
+const trigs = {
+    s: { // 이벤트 설정
+
+    },
+    r: { // 이벤트 해제
+
+    }
+}
+
+/* 통신 객체로 쓰이지 않는 일반적인 데이터들 정의 (warehouse) */
+const wares = {
+    url: window.location.href,
+    params: "",
+    totalRowCount: 0, // 전체 데이터 건수
+    rowCount: 0, // 1페이지에서 보여줄 행 수
+    pageButtonCount: 0, // 페이지 네비게이션에서 보여줄 페이지의 수
+    currentPage: 0, // 현재 페이지
+    totalPage: 0 // 전체 페이지 계산
+}
+
+$(function() { // 페이지가 로드되고 나서 실행
+    onPageLoad();
+});
+
+/* 페이지가 로드되고 나서 실행 될 코드들을 담는다. */
+function onPageLoad() {
+    grids.f.initialization();
+    grids.f.create();
+    grids.t.basicTrigger();
+    getPageNo();
+    createPagingNavigator(wares.currentPage);
+}
+
+function getPageNo() {
+    wares.params = new URL(wares.url).searchParams;
+    if(wares.params.has("page")) {
+        wares.currentPage = parseInt(wares.params.get("page"));
+    } else {
+        wares.currentPage = 1;
+    }
+}
+
+/* 페이지내이션 적절한 값 완성후 wares의 데이터를 이용하도록 수정 */
+var totalRowCount = 500; // 전체 데이터 건수
+var rowCount = 20;	// 1페이지에서 보여줄 행 수
+var pageButtonCount = 10;		// 페이지 네비게이션에서 보여줄 페이지의 수
+var currentPage = 1;	// 현재 페이지
+var totalPage = Math.ceil(totalRowCount / rowCount);	// 전체 페이지 계산
+
+// 페이징 네비게이터를 동적 생성합니다.
+function createPagingNavigator(goPage) {
+	var retStr = "";
+	var prevPage = parseInt((goPage - 1)/pageButtonCount) * pageButtonCount;
+	var nextPage = ((parseInt((goPage - 1)/pageButtonCount)) * pageButtonCount) + pageButtonCount + 1;
+
+	prevPage = Math.max(0, prevPage);
+	nextPage = Math.min(nextPage, totalPage);
+	
+	// 처음
+	retStr += "<a href='javascript:moveToPage(1)'><span class='aui-grid-paging-number aui-grid-paging-first'>first</span></a>";
+
+	// 이전
+	retStr += "<a href='javascript:moveToPage(" + Math.max(1, prevPage) + ")'><span class='aui-grid-paging-number aui-grid-paging-prev'>prev</span></a>";
+
+	for (var i=(prevPage+1), len=(pageButtonCount+prevPage); i<=len; i++) {
+		if (goPage == i) {
+			retStr += "<span class='aui-grid-paging-number aui-grid-paging-number-selected'>" + i + "</span>";
+		} else {
+			retStr += "<a href='javascript:moveToPage(" + i + ")'><span class='aui-grid-paging-number'>";
+			retStr += i;
+			retStr += "</span></a>";
+		}
+		
+		if (i >= totalPage) {
+			break;
+		}
+
+	}
+
+	// 다음
+	retStr += "<a href='javascript:moveToPage(" + nextPage + ")'><span class='aui-grid-paging-number aui-grid-paging-next'>next</span></a>";
+
+	// 마지막
+	retStr += "<a href='javascript:moveToPage(" + totalPage + ")'><span class='aui-grid-paging-number aui-grid-paging-last'>last</span></a>";
+
+	document.getElementById("grid_paging").innerHTML = retStr;
+}
+
+function moveToPage(goPage) {
+	var url = "./taglostlist?page=" + goPage;
+    location.href = url;
+}
+
+
