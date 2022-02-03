9723ee185554dfebdb38660f868d0a15723d09a8
cis-WIn <kr.chis@gmail.com>
2022-02-03 16:49:14 +0900

    설정파일 위치 변경및 application.properties 수정

--- src/main/java/com/broadwave/toppos/configs/WebMvcConfiguration.java
+++ src/main/java/com/broadwave/toppos/configs/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.broadwave.toppos.common;
+package com.broadwave.toppos.configs;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
