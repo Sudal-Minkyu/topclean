d965610dce9c0c508ef585c8f15cfa69159818c4
Ji-Kyum,Kim <ark15963@broadwave.co.kr>
2022-02-08 17:58:09 +0900

    지사출고, 지사출고취소, 가맹점 강제 출고 퍼블리싱 완료 가맹점반송 컨트롤러 추가

--- src/main/java/com/broadwave/toppos/Controller/ManagerController.java
+++ src/main/java/com/broadwave/toppos/Controller/ManagerController.java
@@ -21,6 +21,30 @@ public class ManagerController {
         return "index/managerindex";
     }
 
+    // 출고관리
+    // 지사출고
+    @RequestMapping("release")
+    public String release(){
+        return "manager/releasemanagement/release";
+    }
+    // 지사출고취소
+    @RequestMapping("releasecancel")
+    public String releasecancel(){
+        return "manager/releasemanagement/releasecancel";
+    }
+    // 가맹점강제출고
+    @RequestMapping("forcerelease")
+    public String forcerelease(){
+        return "manager/releasemanagement/forcerelease";
+    }
+
+    // 반송/검품
+    // 가맹점반송
+    @RequestMapping("retturnit")
+    public String retturnit(){
+        return "manager/return/retturnit";
+    }
+
     // 영업일관리 페이지
     @RequestMapping("businessday")
     public String businessday(){
