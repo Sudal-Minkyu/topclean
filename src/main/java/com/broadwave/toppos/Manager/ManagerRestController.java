a0d55d14363dbefd137dda3e7203268077e61061
Minkyu3 <gkhtls2006@broadwave.co.kr>
2022-02-15 13:08:36 +0900

    오타수정2

--- src/main/java/com/broadwave/toppos/Manager/ManagerRestController.java
+++ src/main/java/com/broadwave/toppos/Manager/ManagerRestController.java
@@ -296,4 +296,7 @@ public class ManagerRestController {
     }
 
 
+
+
+
 }
---
+++ src/main/java/com/broadwave/toppos/configs/CachingConfig.java
@@ -0,0 +1,16 @@
+package com.broadwave.toppos.configs;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Minkyu
+ * Date : 2022-02-15
+ * Time :
+ * Remark : Toppos JPA 캐싱관련 Config
+ */
+@EnableCaching
+@Configuration
+public class CachingConfig {
+
+}
