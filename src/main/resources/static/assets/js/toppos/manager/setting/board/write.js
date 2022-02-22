b43fbda68396cbf5faa3dc7fa40c36ac52b53515
snw <snw@broadwave.co.kr>
2022-02-22 09:05:18 +0900

    wrie : 파일 업로드 테스트 준비

--- src/main/resources/static/assets/js/toppos/manager/setting/board/write.js
+++ src/main/resources/static/assets/js/toppos/manager/setting/board/write.js
@@ -14,101 +14,38 @@ const dtos = {
 
 /* 통신에 사용되는 url들 기입 */
 const urls = {
-    
+    save: "/api/",
 }
 
 /* 서버 API를 AJAX 통신으로 호출하며 커뮤니케이션 하는 함수들 (communications) */
 const comms = {
-    
-};
+    save(formData) {
+        console.log(Object.fromEntries(formData));
+        wares.fdata = Object.fromEntries(formData);
 
-/* .s : AUI 그리드 관련 설정들
-*  같은 번호의 배열에 있는 요소들 끼리 철저하게 연동한다는 원칙을 따른다.
-*  어쩔 수 없이 한 그리드에 여러개의 요소가 필요할 경우 다차원 배열을 통해 구현한다.
-*  .f : 그리드 관련 함수들 배치
-* */
-const grids = {
-    s: { // 그리드 세팅
-        targetDiv: [
-            "grid_main"
-        ],
-        columnLayout: [],
-        prop: [],
-        id: [],
-        data: [],
-    },
-
-    f: { // 그리드 펑션
-        initialization() { // 가시성을 위해 grids.s 의 일부 요소를 여기서 선언한다.
-
-            /* 0번 그리드의 레이아웃 */
-            grids.s.columnLayout[0] = [
-                {
-                    dataField: "",
-                    headerText: "",
-                }, {
-                    dataField: "",
-                    headerText: "",
-                },
-            ];
-
-            /* 0번 그리드의 프로퍼티(옵션) 아래의 링크를 참조
-            * https://www.auisoft.net/documentation/auigrid/DataGrid/Properties.html
-            * */
-            grids.s.prop[0] = {
-                editable : false,
-                selectionMode : "singleRow",
-                noDataMessage : "출력할 데이터가 없습니다.",
-                showAutoNoDataMessage: false,
-                enableColumnResize : false,
-                showRowAllCheckBox: false,
-                showRowCheckColumn: false,
-                showRowNumColumn : false,
-                showStateColumn : false,
-                enableFilter : false,
-                rowHeight : 48,
-                headerHeight : 48,
-            };
-        },
-
-        create() { // 그리드 동작 처음 빈 그리드를 생성
-            for (const i in grids.s.columnLayout) {
-                grids.s.id[i] = AUIGrid.create(grids.s.targetDiv[i], grids.s.columnLayout[i], grids.s.prop[i]);
-            }
-        },
-
-        getData(numOfGrid) { // 해당 배열 번호 그리드의 url.read 를 참조하여 데이터를 그리드에 뿌린다.
-            return AUIGrid.getGridData(grids.s.id[numOfGrid]);
-        },
-
-        setData(numOfGrid, data) { // 해당 배열 번호 그리드의 url.read 를 참조하여 데이터를 그리드에 뿌린다.
-            AUIGrid.setGridData(grids.s.id[numOfGrid], data);
-        },
-
-        clearData(numOfGrid) { // 해당 배열 번호 그리드의 데이터 비우기
-            AUIGrid.clearGridData(grids.s.id[numOfGrid]);
-        },
-
-        resize(num) { // 해당 배열 번호 그리드의 크기를 현제 그리드를 감싼 엘리먼트에 맞춰 조절
-			AUIGrid.resize(grids.s.id[num]);
-		},
-
-        getCheckedItems(numOfGrid) { // 해당 배열 번호 그리드의 엑스트라 체크박스 선택된 (아이템 + 행번호) 객체 반환
-            return AUIGrid.getCheckedRowItems(grids.s.id[numOfGrid]);
-        }
-    },
+        // API 시험시 주석을 풀고 urls.save 에 API 주소를 입력
+        // CommonUI.ajax(urls.save, "POST", formData, function (res) {
+        //     console.log(res);
+        // });
+    }
 };
 
 /* 이벤트를 설정하거나 해지하는 함수들을 담는다. */
 const trigs = {
     basic() {
+        $("#fileSelector").on("change", function () {
+            addFile();
+        });
 
+        $("#saveBtn").on("click", function () {
+            saveProgress();
+        });
     },
 }
 
 /* 통신 객체로 쓰이지 않는 일반적인 데이터들 정의 (warehouse) */
 const wares = {
-
+    dataTransfer: new DataTransfer(),
 }
 
 $(function() { // 페이지가 로드되고 나서 실행
@@ -117,7 +54,6 @@ $(function() { // 페이지가 로드되고 나서 실행
 
 /* 페이지가 로드되고 나서 실행 될 코드들을 담는다. */
 function onPageLoad() {
-    grids.f.initialization();
     trigs.basic();
     summernoteInit();
 }
@@ -130,4 +66,20 @@ function summernoteInit() {
         focus: true,
         lang: "ko-KR"
     });
+}
+
+function addFile() {
+    const file = $("#fileSelector")[0].files[0];
+    wares.dataTransfer.items.add(file);
+}
+
+function saveProgress() {
+    const formData = new FormData();
+    formData.set("htSubject", $('#htSubject').val());
+    formData.set("htContent", $('#summernote').summernote('code'));
+    for(let i in wares.dataTransfer.files) {
+        formData.append("files", wares.dataTransfer.files[i]);
+    }
+
+    comms.save(formData);
 }
