7d7935810e756ba0f33e632af3e0ec58b7f4f597
cis-WIn <kr.chis@gmail.com>
2022-03-16 13:51:11 +0900

    RestController AOP 통한 로그 기능 추가

---
+++ src/main/java/com/broadwave/toppos/configs/RestControllerLoggingAspect.java
@@ -0,0 +1,35 @@
+package com.broadwave.toppos.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * @author InSeok
+ * Date : 2022-03-16
+ * Remark :
+ */
+@Aspect
+@Component
+@Slf4j
+public class RestControllerLoggingAspect {
+    @Around("execution(* com.broadwave..*RestController.*(..))")
+    public Object restControllerLogging(ProceedingJoinPoint pjp) throws Throwable {
+
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        Object result = pjp.proceed();
+        stopWatch.stop();
+
+        log.info("[RestController AOP Logging] {}.{} :: {} ms",className,methodName,stopWatch.getTotalTimeMillis());
+        return result;
+    }
+}
