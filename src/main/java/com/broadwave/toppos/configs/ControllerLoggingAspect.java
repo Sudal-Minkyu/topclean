7cc3c014764a950282e6f918f21df607a6ff4be6
cis-WIn <kr.chis@gmail.com>
2022-03-17 09:10:29 +0900

    개발용 property 와 배포영 property 구군, 개발시 sql 로그 가시성좋도록 , param 도 볼수있도록 추가

--- src/main/java/com/broadwave/toppos/configs/ControllerLoggingAspect.java
+++ src/main/java/com/broadwave/toppos/configs/ControllerLoggingAspect.java
@@ -5,8 +5,18 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author InSeok
@@ -20,16 +30,40 @@ public class ControllerLoggingAspect {
     @Around("execution(* com.broadwave..*RestController.*(..))")
     public Object restControllerLogging(ProceedingJoinPoint pjp) throws Throwable {
 
-        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-        String className = methodSignature.getDeclaringType().getSimpleName();
-        String methodName = methodSignature.getName();
-        StopWatch stopWatch = new StopWatch();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        String controllerName = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+        Map<String, Object> params = new HashMap<>();
+        try {
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+            params.put("params", getParams(request));
+            params.put("log_time", LocalDateTime.now());
+            params.put("request_uri", request.getRequestURI());
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         Object result = pjp.proceed();
         stopWatch.stop();
-
-        log.info("[RestController AOP Logging] {}.{} :: {} ms",className,methodName,stopWatch.getTotalTimeMillis());
+        params.put("duration(ms)", stopWatch.getTotalTimeMillis());
+        log.info("[AOP logging of RestController] {}",params);
         return result;
+
+    }
+    private static JSONObject getParams(HttpServletRequest request) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String replaceParam = param.replaceAll("\\.", "-");
+            jsonObject.put(replaceParam, request.getParameter(param));
+        }
+        return jsonObject;
     }
 }
