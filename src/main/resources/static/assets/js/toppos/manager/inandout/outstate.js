48e9b9c17b8ae5abfecb13e1bffe155732895352
snw <snw@broadwave.co.kr>
2022-03-22 09:55:36 +0900

    출고증 관련 버그 수정

--- src/main/resources/static/assets/js/toppos/manager/inandout/outstate.js
+++ src/main/resources/static/assets/js/toppos/manager/inandout/outstate.js
@@ -113,11 +113,11 @@ const comms = {
 
     dispatchPrint(miNoList) {
         dv.chk(miNoList, dtos.send.branchDispatchPrint, "출고증 인쇄를 위한 miNoList 보내기");
-        for(let i=0; i < res.sendData.issueDispatchDtos.length; i++){
-            const frCode = res.sendData.issueDispatchDtos[i]["qrcode"];
-            res.sendData.issueDispatchDtos[i]["qrcode"] = location.protocol+"//"+location.host+"/qrpickup?frcode="+frCode;
-        }
         CommonUI.ajax(urls.dispatchPrint, "GET", miNoList, function (res) {
+            for(let i=0; i < res.sendData.issueDispatchDtos.length; i++){
+                const frCode = res.sendData.issueDispatchDtos[i]["qrcode"];
+                res.sendData.issueDispatchDtos[i]["qrcode"] = location.protocol+"//"+location.host+"/qrpickup?frcode="+frCode;
+            }
             dispatchPrintData(res.sendData.issueDispatchDtos);
         });
     },
