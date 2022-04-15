77255ac3857764c7ca80fd3be290a56e51d62888
CIS-Mac_BW <kr.chis@gmail.com>
2022-04-15 14:40:07 +0900

    P6spy 를 사용하여 Sql 쿼리 로그 개발환경에서 쉽게 볼수있도록 추가

---
+++ src/main/java/com/broadwave/toppos/configs/P6spyConfig.java
@@ -0,0 +1,17 @@
+package com.broadwave.toppos.configs;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class P6spyConfig {
+
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spyPrettySqlFormatterProd.class.getName());
+    }
+}
