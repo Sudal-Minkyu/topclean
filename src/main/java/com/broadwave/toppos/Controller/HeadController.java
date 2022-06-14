602185fd7731445a20cc27701f7f34ca49503dc7
Ji-Kyum,Kim <ark15963@broadwave.co.kr>
2022-06-14 14:41:34 +0900

    본사 정산현황 - 지사 월 정산 입금내역 퍼블리싱

--- src/main/java/com/broadwave/toppos/Controller/HeadController.java
+++ src/main/java/com/broadwave/toppos/Controller/HeadController.java
@@ -215,6 +215,10 @@ public class HeadController {
     @RequestMapping("brmonthbalance")
     public String brmonthbalance(){ return "head/calculate/brmonthbalance"; }
 
+    // 지사 월 정산 입금 등록 내역
+    @RequestMapping("brmonthdeposit")
+    public String brmonthdeposit(){ return "head/calculate/brmonthdeposit"; }
+
     // 지사/가맹점간 정산현황
     // 가맹점 일 정산 현황
     @RequestMapping("frdaybalance")
